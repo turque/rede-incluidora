e89f2fb576da68fc50663c6d6bccec22a730ad8d
F1299859 Arnaldo Davino Godinho Turque <arnaldoturque@bb.com.br>
2024-07-24 14:33:14 -0300

    Iniciando implementação de Design System

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,13 +1,14 @@
 import type { Preview } from "@storybook/react";
 
-import "../app/globals.css"
+import "../app/globals.css";
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
