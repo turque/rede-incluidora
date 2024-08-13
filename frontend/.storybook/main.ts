bb74caeeafa0195f5e173ac23edd62072c33966c
Arnaldo Turque <arnaldo@turque.com.br>
2024-08-12 23:35:03 -0300

    storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,26 +5,21 @@ const config: StorybookConfig = {
     "../components/**/*.mdx",
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: [
-    // "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
+  addons: [// "@storybook/addon-onboarding",
+  "@storybook/addon-links", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions", "@storybook/addon-webpack5-compiler-babel", "@chromatic-com/storybook"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
 };
 
-module.exports = {
-  addons: ['@chakra-ui/storybook-addon'],
-  features: {
-    emotionAlias: false,
-  },
-}
+// module.exports = {
+//   addons: ['@chakra-ui/storybook-addon'],
+//   features: {
+//     emotionAlias: false,
+//   },
+// }
 export default config;
