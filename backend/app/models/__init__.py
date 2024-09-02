59f7f54808fa9b67d686ffc77012b80eb56867d3
Arnaldo Turque <arnaldo@turque.com.br>
2024-09-02 09:18:33 -0300

    Tabela e modelo specialization

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from .message import *  # noqa
 from .security import *  # noqa
 from .users import *  # noqa
 from .professional import *  # noqa
+from .specialization import *  # noqa
--- backend/app/models/specialization.py
+++ backend/app/models/specialization.py
@@ -1,7 +1,9 @@
 from datetime import date
-from sqlmodel import Field, Relationship, SQLModel, Date
+
+from sqlmodel import Field, Relationship, SQLModel
 
 from .professional import Professional
+
 
 # Shared properties
 class SpecializationBase(SQLModel):
@@ -9,7 +11,7 @@ class SpecializationBase(SQLModel):
     description: str | None = Field(default=None, max_length=1024)
     certification: str | None = Field(default=None, max_length=512)
     institution: str | None = Field(default=None, max_length=512)
-    year_obtained:  Date | None = Field(default=None)
+    year_obtained: date | None = Field(default=None)
 
 
 # Properties to receive via API on creation
@@ -24,7 +26,11 @@ class SpecializationUpdate(SpecializationBase):
 
 # Database model, database table inferred from class name
 class Specialization(SpecializationBase, table=True):
-    pass
+    id: int | None = Field(default=None, primary_key=True)
+    prefessional_id: int | None = Field(
+        default=None, foreign_key="professional.id", nullable=False
+    )
+    professional: "Professional" = Relationship(back_populates="professional_data")
 
 
 # Properties to return via API, id is always required
