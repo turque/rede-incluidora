b2c4a1b41212d4496997820b0fb7ffb39028b75f
Arnaldo Turque <arnaldo@turque.com.br>
2024-11-29 15:56:53 -0300

    Ajustes na página principal

--- frontend/app/(pesquisa)/page.tsx
+++ frontend/app/(pesquisa)/page.tsx
@@ -1,10 +1,15 @@
-import { Box, Container, Text } from '@chakra-ui/react';
+import { Box, Container, Text, Flex } from '@chakra-ui/react';
 import SearchBar from '@/components/SearchBar';;
 
 export default function Home() {
   return (
     <Container maxW="container.xl" py={8}>
       <Box as="main">
+      {/* <Flex maxW="300px"> */}
+        <Text fontSize="3xl" color="orange.400" textAlign="left" mb={8} fontWeight="bold" lineClamp="2">
+          Pesquise por especialistas e encontre o melhor atendimento para você.
+        </Text>
+        {/* </Flex> */}
         <SearchBar />
       </Box>
     </Container>
