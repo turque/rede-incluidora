032de047458db8f87f320f44e86fb4b765e37903
Arnaldo Turque <arnaldo@turque.com.br>
2024-11-29 15:58:59 -0300

    Ajustes na página principal

--- frontend/app/(pesquisa)/page.tsx
+++ frontend/app/(pesquisa)/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
   return (
     <Container maxW="container.xl" py={8}>
       <Box as="main">
-      {/* <Flex maxW="300px"> */}
-        <Text fontSize="3xl" color="orange.400" textAlign="left" mb={8} fontWeight="bold" lineClamp="2">
+        <Text fontSize="3xl" color="orange.400" textAlign="left" mb={8} fontWeight="bold">
           Pesquise por especialistas e encontre o melhor atendimento para você.
         </Text>
-        {/* </Flex> */}
         <SearchBar />
       </Box>
     </Container>
